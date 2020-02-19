05ad3db2ef6ae076f49d10dc04d95501e549496b
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2020-02-19 08:20:25 +0800

    remove bluenoise sampler

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 [![codecov.io](http://codecov.io/github/baggepinnen/Hyperopt.jl/coverage.svg?branch=master)](http://codecov.io/github/baggepinnen/Hyperopt.jl?branch=master)
 
 
-A package to perform hyperparameter optimization. Currently supports random search, [latin hypercube sampling](https://en.wikipedia.org/wiki/Latin_hypercube_sampling), [Bayesian optimization](https://arxiv.org/abs/1807.02811) and blue-noise search.
+A package to perform hyperparameter optimization. Currently supports random search, [latin hypercube sampling](https://en.wikipedia.org/wiki/Latin_hypercube_sampling) and [Bayesian optimization](https://arxiv.org/abs/1807.02811).
 
 # Usage
 
@@ -29,7 +29,7 @@ end
 ## Details
 
 1. The macro `@hyperopt` takes a for-loop with an initial argument determining the number of samples to draw (`i` below).
-2. The sample strategy can be specified by specifying the special keyword `sampler = Sampler(opts...)`. Available options are `RandomSampler()`, `LHSampler()`, `CLHSampler(dims=[Continuous(), Categorical(2), Continuous(), ...])`, `BlueNoiseSampler()`, `Hyperband(R=50, η=3, inner=RandomSampler())` and `GPSampler(Min)/GPSampler(Max)`.
+2. The sample strategy can be specified by specifying the special keyword `sampler = Sampler(opts...)`. Available options are `RandomSampler()`, `LHSampler()`, `CLHSampler(dims=[Continuous(), Categorical(2), Continuous(), ...])`, `Hyperband(R=50, η=3, inner=RandomSampler())` and `GPSampler(Min)/GPSampler(Max)`.
 3. The subsequent arguments to the for-loop specifies names and candidate values for different hyper parameters (`a = LinRange(1,2,1000), b = [true, false], c = exp10.(LinRange(-1,3,1000))` below).
 4. A useful strategy to achieve log-uniform sampling is logarithmically spaced vector, e.g. `c = exp10.(LinRange(-1,3,1000))`.
 5. In the example below, the parameters `i,a,b,c` can be used within the expression sent to the macro and they will hold a new value sampled from the corresponding candidate vector each iteration.
@@ -125,7 +125,7 @@ end
 ```
 
 # Categorical variables
-`RandomSampler` `CLHSampler` and `BlueNoiseSampler` support categorical variables which do not have a natural floating point representation, such as functions:
+`RandomSampler` and `CLHSampler` support categorical variables which do not have a natural floating point representation, such as functions:
 ```julia
 @hyperopt for i=20, fun = [tanh, σ, relu]
     train_network(fun)
@@ -137,14 +137,13 @@ end
     train_network(fun, param)
 end
 ```
-Caveat for `BlueNoiseSampler`: see below.
 
 # Which sampler to use?
 `RandomSampler` is a good baseline and the default if none is chosen. `GPSampler` fits a Gaussian process to the data and tries to use this model to figure out where the best point to sample next is (using expected improvement). This is somewhat expensive and pays off when the function to optimize is expensive. `Hyperband(R=50, η=3, inner=RandomSampler())` runs the expression with varying amount of resources, allocating more resources to promising hyperparameters. See below for more info on `Hyperband`.
 
-If number of iterations is small, `BlueNoiseSampler` or `LHSampler` work better than random search. Caveat: `BlueNoiseSampler` and `LHSampler` need all candidate vectors to be of equal length, i.e.,
+If number of iterations is small, `LHSampler` work better than random search. Caveat: `LHSampler` needs all candidate vectors to be of equal length, i.e.,
 ```julia
-hob = @hyperopt for i=100, sampler=BlueNoiseSampler(),
+hob = @hyperopt for i=100, sampler = LHSampler(),
                             a = LinRange(1,5,100),
                             b = repeat([true, false],50),
                             c = exp10.(LinRange(-1,3,100))
@@ -164,11 +163,6 @@ hob = @hyperopt for i=100,
 end
 ```
 
-`BlueNoiseSampler` performs an optimization problem in the beginning to spread out samples so as to sample the space as evenly as possible, both as measured in the full dimensional space, and in each dimension separately. Inspiration for this sampler comes from [Projective Blue-Noise Sampling](http://resources.mpi-inf.mpg.de/ProjectiveBlueNoise/ProjectiveBlueNoise.pdf) but the implemented algorithm is not the same.
-The result of the blue noise optimization in 2 dimensions is visualized below. Initial values to the left and optimized to the right.
-![window](bluenoise.png)
-
-If the number of iterations is very large, the optimization problem might take long time to run in comparison to the runtime of a single experiment and random sampling will end up more effective.
 
 ## Hyperband
 `Hyperband(R=50, η=3, inner=RandomSampler())` Implements [Hyperband: A Novel Bandit-Based Approach to Hyperparameter Optimization](https://arxiv.org/abs/1603.06560). The maximum amount of resources is given by `R` and the parameter `η` roughly determines the proportion of trials discarded between each round of successive halving. When using `Hyperband` the expression inside the `@hyperopt` macro takes the following form
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,97 +1,87 @@
-using Test, Random, Distributed
+using Test, Random, Distributed, Statistics
 Random.seed!(0)
 using Hyperopt, Plots
+f(a,b=true;c=10) = sum(@. 100 + (a-3)^2 + (b ? 10 : 20) + (c-100)^2) # This function must be defined outside testsets to avoid scoping issues
 
 @testset "Hyperopt" begin
-    f(a,b=true;c=10) = sum(@. 100 + (a-3)^2 + (b ? 10 : 20) + (c-100)^2)
-
-    # res = map(1:30) do i
-    #     @info("Iteration ", i)
-    hor = @hyperopt for i=100, sampler=RandomSampler(), a = LinRange(1,5,50), b = [true, false], c = exp10.(LinRange(-1,3,50))
-        # println(i, "\t", a, "\t", b, "\t", c)
-        # print(i, " ")
-        f(a,b,c=c)
-    end
-    @test minimum(hor)[2] < 300
-    @test length(hor.history) == 100
-    @test length(hor.results) == 100
-    @test all(hor.history) do h
-        all(hi in hor.candidates[i] for (i,hi) in enumerate(h))
-    end
-
-    horp = @phyperopt for i=100, sampler=RandomSampler(), a = LinRange(1,5,50), b = [true, false], c = exp10.(LinRange(-1,3,50))
-        # println(i, "\t", a, "\t", b, "\t", c)
-        # print(i, " ")
-        f(a,b,c=c)
-    end
-    @test minimum(horp)[2] < 300
-    @test length(horp.history) == 100
-    @test length(horp.results) == 100
-
-    addprocs(2)
-    @everywhere using Hyperopt
-    @everywhere f(a,b=true;c=10) = sum(@. 100 + (a-3)^2 + (b ? 10 : 20) + (c-100)^2)
-    horp = @phyperopt for i=100, sampler=RandomSampler(), a = LinRange(1,5,50), b = [true, false], c = exp10.(LinRange(-1,3,50))
-        # println(i, "\t", a, "\t", b, "\t", c)
-        # print(i, " ")
-        f(a,b,c=c)
-    end
-    @test minimum(horp)[2] < 300
-    @test length(horp.history) == 100
-    @test length(horp.results) == 100
-
-    hob = @hyperopt for i=100, sampler=BlueNoiseSampler(), a = LinRange(1,5,100), b = repeat([true, false],50), c = exp10.(LinRange(-1,3,100))
-        # println(i, "\t", a, "\t", b, "\t", c)
-        # print(i, " ")
-        f(a,b,c=c)
-    end
-    @test minimum(hob)[2] < 300
-
-    hol = @hyperopt for i=100, sampler=LHSampler(), a = LinRange(1,5,100), b = repeat([true, false],50), c = exp10.(LinRange(-1,3,100))
-        # println(i, "\t", a, "\t", b, "\t", c)
-        # print(i, " ")
-        f(a,b,c=c)
-    end
-    @test minimum(hol)[2] < 300
-
-    hocl = @hyperopt for i=100, sampler=CLHSampler(dims=[Continuous(),Categorical(2),Continuous()]), a = LinRange(1,5,100), b = [true, false], c = exp10.(LinRange(-1,3,100))
-        # println(i, "\t", a, "\t", b, "\t", c)
-        # print(i, " ")
-        f(a,b,c=c)
-    end
-    @test minimum(hocl)[2] < 300
 
 
-    hogp = @hyperopt for i=50, sampler=GPSampler(Min), a = LinRange(1,5,100), b = repeat([true, false]',50)[:], c = exp10.(LinRange(-1,3,100))
-        # println(i, "\t", a, "\t", b, "\t", c)
-        # print(i, " ")
-        f(a,Bool(b),c=c)
-    end
-    @test minimum(hogp)[2] < 300
+    @testset "Random sampler" begin
+        @info "Testing Random sampler"
 
-    plot(hogp.sampler)
-    plot(hogp)
-
-
-
-
-    # Error handling
-    @test_throws ArgumentError @hyperopt for i=100, sampler=BlueNoiseSampler(), a = LinRange(1,5,100), b = [true, false], c = exp10.(LinRange(-1,3,100))
-        # println(i, "\t", a, "\t", b, "\t", c)
-        # print(i, " ")
-        f(a,b,c=c)
+        # res = map(1:30) do i
+        #     @info("Iteration ", i)
+        hor = @hyperopt for i=100, sampler=RandomSampler(), a = LinRange(1,5,50), b = [true, false], c = exp10.(LinRange(-1,3,50))
+            # println(i, "\t", a, "\t", b, "\t", c)
+            f(a,b,c=c)
+        end
+        @test minimum(hor)[2] < 300
+        @test length(hor.history) == 100
+        @test length(hor.results) == 100
+        @test all(hor.history) do h
+            all(hi in hor.candidates[i] for (i,hi) in enumerate(h))
+        end
     end
 
-    @test_throws ArgumentError @hyperopt for i=100, sampler=LHSampler(), a = LinRange(1,5,10), b = repeat([true, false],50), c = exp10.(LinRange(-1,3,100))
-        # println(i, "\t", a, "\t", b, "\t", c)
-        # print(i, " ")
-        f(a,b,c=c)
+    @testset "Latin hypercube" begin
+        @info "Testing Latin hypercube"
+        hol = @hyperopt for i=100, sampler=LHSampler(), a = LinRange(1,5,100), b = repeat([true, false],50), c = exp10.(LinRange(-1,3,100))
+            # println(i, "\t", a, "\t", b, "\t", c)
+            f(a,b,c=c)
+        end
+        @test minimum(hol)[2] < 300
+
+
+        hocl = @hyperopt for i=100, sampler=CLHSampler(dims=[Continuous(),Categorical(2),Continuous()]), a = LinRange(1,5,100), b = [true, false], c = exp10.(LinRange(-1,3,100))
+            # println(i, "\t", a, "\t", b, "\t", c)
+            f(a,b,c=c)
+        end
+        @test minimum(hocl)[2] < 300
+
     end
 
-    @test_throws ArgumentError @hyperopt for i=100, sampler=CLHSampler(dims=[Continuous(),Categorical(2),Continuous()]), a = LinRange(1,5,99), b = [true, false], c = exp10.(LinRange(-1,3,100))
-        # println(i, "\t", a, "\t", b, "\t", c)
-        # print(i, " ")
-        f(a,b,c=c)
+    @testset "GP sampler" begin
+        @info "Testing GP sampler"
+
+        results = map(1:20) do _
+            hogp = @hyperopt for i=50, sampler=GPSampler(Min), a = LinRange(1,5,100), b = repeat([true, false]',50)[:], c = exp10.(LinRange(-1,3,100))
+                # println(i, "\t", a, "\t", b, "\t", c)
+                f(a,Bool(b),c=c)
+            end
+            minimum(hogp)[2]
+        end
+
+        @show mean(results)
+        @show mean(results .< 300)
+
+        @test mean(results) < 300
+        @test mean(results .< 300) > 0.8
+
+
+        hogp = @hyperopt for i=50, sampler=GPSampler(Min), a = LinRange(1,5,100), b = repeat([true, false]',50)[:], c = exp10.(LinRange(-1,3,100))
+            f(a,Bool(b),c=c)
+        end
+        minimum(hogp)[2]
+
+        plot(hogp.sampler)
+        plot(hogp)
+
+    end
+
+
+    @testset "Error handling" begin
+        @info "Testing Error handling"
+
+
+        @test_throws ArgumentError @hyperopt for i=100, sampler=LHSampler(), a = LinRange(1,5,10), b = repeat([true, false],50), c = exp10.(LinRange(-1,3,100))
+            # println(i, "\t", a, "\t", b, "\t", c)
+            f(a,b,c=c)
+        end
+
+        @test_throws ArgumentError @hyperopt for i=100, sampler=CLHSampler(dims=[Continuous(),Categorical(2),Continuous()]), a = LinRange(1,5,99), b = [true, false], c = exp10.(LinRange(-1,3,100))
+            # println(i, "\t", a, "\t", b, "\t", c)
+            f(a,b,c=c)
+        end
     end
 
     #     minimum.((hor,hob,hot,hof))
@@ -101,43 +91,75 @@ using Hyperopt, Plots
     # hor,hob,hot,hof = getindex.(hor,2),getindex.(hob,2),getindex.(hot,2),getindex.(hof,2)
     # res = [hor hob hot hof]
     # sum(log.(res), dims=1)
+    @testset "Manual" begin
+        @info "Testing Manual"
 
-    ho = Hyperoptimizer(10, a = range(1, stop=2, length=50), b = [true, false], c = randn(100))
-    for (i,a,b,c) in ho
-        println(i, "\t", a, "\t", b, "\t", c)
-    end
-
-
-
-    ho = @hyperopt for i=100, a = LinRange(1,5,50), b = [true, false], c = exp10.(LinRange(-1,3,50)), d = [tanh, exp]
-        # println(i, "\t", a, "\t", b, "\t", c)
-        # print(i, " ")
-        f(a,b,c=c) + d(a)
-    end
-end
-
-@testset "Utils" begin
-    @test Hyperopt.islogspace(exp10.(LinRange(-3, 3, 10)))
-    @test !Hyperopt.islogspace(LinRange(-3, 3, 10))
-    @test !Hyperopt.islogspace([true, false])
-end
-
-
-@testset "Hyperband" begin
-    using Optim
-    f(a;c=10) = sum(@. 100 + (a-3)^2 + (c-100)^2)
-    Hyperopt.Hyperband(10)
-    # res = map(1:30) do i
-    #     @info("Iteration ", i)
-    hohb = @hyperopt for i=18, sampler=Hyperband(R=50, η=3, inner=RandomSampler()), a = LinRange(1,5,1800), c = exp10.(LinRange(-1,3,1800))
-        # println(i, "\t", a, "\t", b, "\t", c)
-        # print(i, " ")
-        if !(state === nothing)
-            a,c = state
+        ho = Hyperoptimizer(10, a = range(1, stop=2, length=50), b = [true, false], c = randn(100))
+        for (i,a,b,c) in ho
+            println(i, "\t", a, "\t", b, "\t", c)
         end
-        res = Optim.optimize(x->f(x[1],c=x[2]), [a,c], SimulatedAnnealing(), Optim.Options(f_calls_limit=i))
-        Optim.minimum(res), Optim.minimizer(res)
+
     end
-    @test minimum(hohb)[2] < 300
+
+
+
+    @testset "Categorical" begin
+        @info "Testing Categorical"
+
+        ho = @hyperopt for i=100, a = LinRange(1,5,50), b = [true, false], c = exp10.(LinRange(-1,3,50)), d = [tanh, exp]
+            # println(i, "\t", a, "\t", b, "\t", c)
+            f(a,b,c=c) + d(a)
+        end
+    end
+
+
+    @testset "Utils" begin
+        @test Hyperopt.islogspace(exp10.(LinRange(-3, 3, 10)))
+        @test !Hyperopt.islogspace(LinRange(-3, 3, 10))
+        @test !Hyperopt.islogspace([true, false])
+    end
+
+
+    @testset "Hyperband" begin
+        using Optim
+        f(a;c=10) = sum(@. 100 + (a-3)^2 + (c-100)^2)
+        Hyperopt.Hyperband(10)
+        # res = map(1:30) do i
+        #     @info("Iteration ", i)
+        hohb = @hyperopt for i=18, sampler=Hyperband(R=50, η=3, inner=RandomSampler()), a = LinRange(1,5,1800), c = exp10.(LinRange(-1,3,1800))
+            # println(i, "\t", a, "\t", b, "\t", c)
+            if !(state === nothing)
+                a,c = state
+            end
+            res = Optim.optimize(x->f(x[1],c=x[2]), [a,c], SimulatedAnnealing(), Optim.Options(f_calls_limit=i))
+            Optim.minimum(res), Optim.minimizer(res)
+        end
+        @test minimum(hohb)[2] < 300
+
+    end
+
+    @testset "Parallel" begin
+        @info "Testing Parallel"
+
+        horp = @phyperopt for i=100, sampler=RandomSampler(), a = LinRange(1,5,50), b = [true, false], c = exp10.(LinRange(-1,3,50))
+            # println(i, "\t", a, "\t", b, "\t", c)
+            f(a,b,c=c)
+        end
+        @test minimum(horp)[2] < 300
+        @test length(horp.history) == 100
+        @test length(horp.results) == 100
+
+        Distributed.nworkers() == 0 && addprocs(2)
+        @everywhere using Hyperopt
+        @everywhere f(a,b=true;c=10) = sum(@. 100 + (a-3)^2 + (b ? 10 : 20) + (c-100)^2)
+        horp = @phyperopt for i=100, sampler=RandomSampler(), a = LinRange(1,5,50), b = [true, false], c = exp10.(LinRange(-1,3,50))
+            # println(i, "\t", a, "\t", b, "\t", c)
+            f(a,b,c=c)
+        end
+        @test minimum(horp)[2] < 300
+        @test length(horp.history) == 100
+        @test length(horp.results) == 100
+
+    end
 
 end
